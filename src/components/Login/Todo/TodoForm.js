37d08f0bed0b22d90c6f0fd9bcdd18c0deea0c9b
abdulhak99m <abdulhakeemimran@hotmail.com>
2021-06-14 01:13:20 +0100

    adding parameter

--- src/components/Login/Todo/TodoForm.js
+++ src/components/Login/Todo/TodoForm.js
@@ -43,7 +43,23 @@ function TodoForm(props) {
       ) : (
         <>
           <input
-            placeholder='Add a todo'
+            placeholder='Package Name'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <input
+            placeholder='From'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+           <input
+            placeholder='To'
             value={input}
             onChange={handleChange}
             name='text'
