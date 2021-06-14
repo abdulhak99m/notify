95be46d32f8503c43f4e10bbcd391ddb2a670e4d
abdulhak99m <abdulhakeemimran@hotmail.com>
2021-06-14 03:15:59 +0100

    another one

--- src/components/Login/Todo/TodoForm.js
+++ src/components/Login/Todo/TodoForm.js
@@ -3,15 +3,17 @@ import React, { useState, useEffect, useRef } from 'react';
 
 function TodoForm(props) {
   const [input, setInput] = useState(props.edit ? props.edit.value : '');
+  const [from, setFrom] = useState('');
   
   const inputRef = useRef(null);
-
+  const fromRef = useRef(null);
   useEffect(() => {
     inputRef.current.focus();
   });
 
   const handleChange = e => {
     setInput(e.target.value);
+    setFrom(e.target.from);
   };
 
   const handleSubmit = e => {
@@ -20,9 +22,10 @@ function TodoForm(props) {
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
       text: input,
-     
+     text2:from
     });
     setInput('');
+    setFrom('');
   };
 
   return (
@@ -51,7 +54,14 @@ function TodoForm(props) {
             className='todo-input'
             ref={inputRef}
           />
-         
+            <input
+            placeholder='From'
+            value={from}
+            onChange={handleChange}
+            name='text2'
+            className='todo-input'
+            ref={fromRef}
+          />
            
           <button onClick={handleSubmit} className='todo-button'>
           Post my Package
