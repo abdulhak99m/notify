cdd03d87e3acd392db7fd52fe07920903d011ac9
abdulhak99m <abdulhakeemimran@hotmail.com>
2021-06-15 13:53:05 +0100

    wefsd

--- src/components/Login/Todo/TodoForm.js
+++ src/components/Login/Todo/TodoForm.js
@@ -29,7 +29,7 @@ function TodoForm(props) {
 
   const handleSubmit = e => {
     e.preventDefault();
-    alert('A name was submitted2: ');
+    alert('A name was submitted2: ' + this.theForm);
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
       
