a75132f7020f5d61baabce9410f9379fc3391f96
abdulhak99m <abdulhakeemimran@hotmail.com>
2021-06-17 23:46:02 +0100

    jjj

--- src/components/Login/Todo/TodoForm.js
+++ src/components/Login/Todo/TodoForm.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect, useRef } from 'react';
 
 function TodoForm(props) {
   const [input, setInput] = useState(props.edit ? props.edit.value : '');
-
+  const [input2, setInput2] = useState(props.edit ? props.edit.value : '');
+  
   const inputRef = useRef(null);
 
   useEffect(() => {
@@ -13,15 +14,21 @@ function TodoForm(props) {
   const handleChange = e => {
     setInput(e.target.value);
   };
+  const handleChange2 = e => {
+    setInput2(e.target.value);
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
 
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
-      text: input
+      text: input,
+      text2: input2
+
     });
     setInput('');
+    setInput2('');
   };
 
   return (
@@ -47,6 +54,14 @@ function TodoForm(props) {
             value={input}
             onChange={handleChange}
             name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <input
+            placeholder='FroM'
+            value={input2}
+            onChange={handleChange2}
+            name='text2'
             className='todo-input'
             ref={inputRef}
           />
